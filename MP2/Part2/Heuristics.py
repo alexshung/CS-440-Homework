4e9e378ad7cc22c012c4df51870771bf0c2ceb46
alex.shung <alex.shung@workday.com>
2017-10-26 21:45:12 -0700

    Make offensive heuristic smarter

--- MP2/Part2/Heuristics.py
+++ MP2/Part2/Heuristics.py
@@ -41,24 +41,25 @@ def offensiveHeuristic2(board, color, depth, isBoardWon, isMax):
 		score = -1 * maxScore + depth if isMax else maxScore - depth
 	else:
 		numPiecesLeft = len(board.black) if color == wp else len(board.white)
-		basicHeur = 2 * (30 - numPiecesLeft)
-		positionScore = getFarthestPiece(board, color)
+		basicHeur = 3 * (30 - numPiecesLeft)
+		positionScore = getFarthestPiece(board, color, True)
 
 		oppColor = wp if color == bp else bp
 		opponentScore = offensiveHeuristic1(board, oppColor)
-		oppPositionScore = getFarthestPiece(board, oppColor)
-		score = basicHeur + r.random() - depth + positionScore - opponentScore #- oppPositionScore
+		# oppPositionScore = getFarthestPiece(board, oppColor)
+		score = basicHeur + r.random() - depth + positionScore * 2 - opponentScore #- oppPositionScore
 	return score
 
 # This returns the farthest y coordinate of the piece
 # For black that will be the greatest y, for white it will be the least
-def getFarthestPiece(board, color):
-	isMax = True if color == bp else False
+def getFarthestPiece(board, color, isOffensive = False):
+	isBlack = True if color == bp else False
 	myPieces = board.black if color == bp else board.white
 	farthest = None
 	for piece in myPieces:
-		curPos = piece.y #* 2 + piece.x
-		isBetter = True if not farthest else curPos > farthest if isMax else curPos < farthest
+		curPos = piece.y if not isBlack else 8 - piece.y #* 2 + piece.x
+		isBetter = True if not farthest else curPos > farthest if isOffensive else curPos < farthest
 		if isBetter:
 			farthest = curPos
 	return farthest
+
