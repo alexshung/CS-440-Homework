004dacc32815b2f2fe8d71298d632e6279e92a6c
alex.shung <alex.shung@workday.com>
2017-11-14 12:36:42 -0800

    MP3 Part 1 Finish

--- MP3/Part1/MP3.py
+++ MP3/Part1/MP3.py
@@ -2,6 +2,7 @@ import numpy as np
 from collections import Counter, defaultdict
 from math import log
 from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 
 
 def convertToNums(x):
@@ -133,7 +134,7 @@ def testing(imageFilePath, classToFeatures, classPriors):
 
 def evaluation(results, testLabelFilePath):
 	testLabels = [int(x) for x in readLabels(testLabelFilePath)]
-	confusionMatrix = confusion_matrix(testLabels, results)
+	confusionMatrix = confusion_matrix(testLabels, results, labels = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
 	resultsMap = {}
 	correctHeur = 0
@@ -148,7 +149,8 @@ def evaluation(results, testLabelFilePath):
 		resultsMap[i] = (correct / totalClassified) * 100
 		correctHeur += correct
 
-	print(f"Classification rate for digit. Format Class: % {resultsMap}")
+	for key in resultsMap:
+		print(f"Class {key}: {resultsMap[key]}% correct")
 	print(confusionMatrix)
 	return (confusionMatrix, correctHeur)
 
@@ -164,6 +166,7 @@ def printBestWorstForClass(imageFilePath, bestWorstClassTracker):
 		print(f"Worst")
 		for line in numbers[worst]:
 			print(''.join(str(line)))
+
 def findBestSmoothingConstant(imageFilePath, labelFilePath):
 	bestHeur = None
 	bestK = None
@@ -180,6 +183,41 @@ def findBestSmoothingConstant(imageFilePath, labelFilePath):
 			bestHeur = correctHeur
 
 	return bestK
+
+def oddsRatioMaker(classToFeatures):
+	# Makes ratio for 4 most incorrectly classified pairs
+	# 7 as 9: 14 times
+	# 4 as 9: 18 times
+	# 8 as 3: 14 times
+	# 5 as 3: 12 times
+	pairs = [(7, 9), (4, 9), (8, 3), (5, 3)]
+	i = 1
+	for pair in pairs:
+		x = str(pair[0])
+		y = str(pair[1])
+		
+		fig = plt.figure(i)
+		i += 1
+
+		mx = np.log(classToFeatures[x])
+		my = np.log(classToFeatures[y])
+		oddsRatio = mx - my
+
+		figx = fig.add_subplot(1, 3, 1)
+		figx.imshow(mx, aspect='auto', cmap = 'RdYlBu', interpolation='nearest')
+		figx.set_title(f"{x}")
+
+		figy = fig.add_subplot(1, 3, 2)
+		figy.imshow(my, aspect='auto', cmap = 'RdYlBu', interpolation='nearest')
+		figy.set_title(f"{y}")
+		
+		figxy = fig.add_subplot(1, 3, 3)
+		img = figxy.imshow(oddsRatio, aspect='auto', cmap = 'RdYlBu', interpolation='nearest')
+		figxy.set_title(f"{x} over {y}")
+
+		fig.colorbar(img)
+	plt.show()
+
 
 if __name__ == "__main__":
 	dirPath = "digitdata/"
@@ -199,7 +237,8 @@ if __name__ == "__main__":
 	(testResults, bestWorstClassTracker) = testing(fullTestFileName, classToFeatures, classPriors)
 
 	(c_matrix, correctHeur) = evaluation(testResults, labelTestFilePath)
-	print(correctHeur)
-	# printBestWorstForClass(fullTestFileName, bestWorstClassTracker)
-	
+
+	# oddsRatioMaker(classToFeatures)
+	# print(correctHeur)
+	printBestWorstForClass(fullTestFileName, bestWorstClassTracker)
 
